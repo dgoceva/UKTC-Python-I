c6d5f17ab5b79296311acf3021f00826d3c9ea1e
dgoceva <44052125+dgoceva@users.noreply.github.com>
2021-10-07 10:43:01 +0300

    prints&perimeter&square

---
+++ print_formats.py
@@ -0,0 +1,16 @@
+a = int(input('a= '))
+b = int(input('b= '))
+c = int(input('c= '))
+
+p = a+b+c
+print('P= ', p)
+print('P= '+str(p))
+print('P= {0}'.format(p))
+print('P= %d' % (p))
+print(f'P= {p}')
+
+a = float(input('a= '))
+b = float(input('b= '))
+h = float(input('h= '))
+s = (a+b)/2*h
+print('S = '+str(s))
