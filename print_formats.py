15407036bb37f2268484ce4ffc9d137247b8e45d
dgoceva <44052125+dgoceva@users.noreply.github.com>
2021-10-07 10:45:59 +0300

    ceil&floor&round

--- print_formats.py
+++ print_formats.py
@@ -1,3 +1,5 @@
+import math
+
 a = int(input('a= '))
 b = int(input('b= '))
 c = int(input('c= '))
@@ -14,3 +16,7 @@ b = float(input('b= '))
 h = float(input('h= '))
 s = (a+b)/2*h
 print('S = '+str(s))
+
+print(math.ceil(34.25))
+print(math.floor(34.25))
+print(round(23.54, 1))
