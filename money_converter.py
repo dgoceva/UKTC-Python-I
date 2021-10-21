e79796790fca69243624709cae1f499c1d413eeb
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-21 10:26:47 +0300

    convertion to BGN

--- money_converter.py
+++ money_converter.py
@@ -1,2 +1,11 @@
 amount = float(input('Amount = '))
-print('USD {0} = {1} BGN'.format(amount, round(amount*1.79549, 2)))
+money = input('Money = ')
+
+if money == 'USD':
+    print('USD {0} = {1} BGN'.format(amount, round(amount*1.79549, 2)))
+elif money == 'EUR':
+    print('EUR {0} = {1} BGN'.format(amount, round(amount*1.95583, 2)))
+elif money == 'GBP':
+    print('GBP {0} = {1} BGN'.format(amount, round(amount*2.53405, 2)))
+else:
+    print('Error')
