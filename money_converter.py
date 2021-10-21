650be1ce6758fcb419a791d4cbcef14ffe724c14
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-21 10:20:52 +0300

    rectangle and USD to BGN

---
+++ money_converter.py
@@ -0,0 +1,2 @@
+amount = float(input('Amount = '))
+print('USD {0} = {1} BGN'.format(amount, round(amount*1.79549, 2)))
---
+++ reactangle.py
@@ -0,0 +1,8 @@
+x1 = int(input('x1='))
+y1 = int(input('y1='))
+x2 = int(input('x2='))
+y2 = int(input('y2='))
+a = max(x1, x2)-min(x1, x2)
+b = max(y1, y2)-min(y1, y2)
+print('P=', (2*(a+b)))
+print('S=', a*b)
