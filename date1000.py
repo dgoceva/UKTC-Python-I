e9b059588a1758b67e717b8fd6719536c85fa067
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-28 10:34:23 +0300

    date1000 with strptime&timedelta

--- date1000.py
+++ date1000.py
@@ -1,13 +1,6 @@
 import datetime as dt
 
-print(dt.datetime.now())
-
-day = int(input('[1-31]: '))
-month = int(input('1-12: '))
-year = int(input('year: '))
-
-#  1000 = 2 years + 9 months + 0 days
-
-input_date = dt.datetime(year, month, day)
-print(input_date)
-print(input_date+dt.timedelta(days=1000))
+str_date = input('Input date[dd-mm-yyyy]: ')
+input_date = dt.datetime.strptime(str_date, '%d-%m-%Y')
+out_date = input_date+dt.timedelta(days=1000)
+print(out_date.strftime('%d-%m-%Y'))
