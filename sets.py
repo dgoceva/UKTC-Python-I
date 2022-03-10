59d432df4ca23f602aef9dec11fc715da4f57f4f
dgoceva <44052125+dgoceva@users.noreply.github.com>
2022-03-10 10:45:36 +0200

    tuples and sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+s1 = set()
+s2 = frozenset([1, 2, 3, 4])
+print(s1, s2)
+s1 = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+print(s1)
+
+print(len(s1), len(s2))
+
+s2 = frozenset((1, 2, 3, 4, 5, 6))
+print(s2)
